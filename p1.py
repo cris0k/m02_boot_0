9850b40aa4b64b3696fbc0afacdd08ff9a7b608e
cristina <criskukhar@hotmail.com>
2022-01-24 21:28:17 +0100

    funciones clasecas

--- p1.py
+++ p1.py
@@ -5,4 +5,12 @@ def sumaTodos(limitTo):
         
     return resultado
 
+def sumaTodosCuadrados(limitTo):
+    resultado = 0
+    for i in range(limitTo + 1):
+        resultado += i*i
+        
+    return resultado
+
 print(sumaTodos(100))
+print(sumaTodosCuadrados(3))
