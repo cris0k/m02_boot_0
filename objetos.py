7ccdd17fc83af06f9c1fb75efb08433c70d247ef
cristina <criskukhar@hotmail.com>
2022-01-26 19:29:46 +0100

    objetos

--- objetos.py
+++ objetos.py
@@ -2,10 +2,18 @@ import turtle
 
 tortuguita = turtle.Turtle()
 otraTortuguita = turtle.Turtle()
+lentorra = turtle.Turtle()
 
 tortuguita.shape('turtle')
-tortuguita.fd(50)
+tortuguita.color('blue')
+tortuguita.fd(100)
+tortuguita.speed(5)
 
 otraTortuguita.color('green')
 otraTortuguita.left(90)
-otraTortuguita.fd(50)
+otraTortuguita.fd(100)
+otraTortuguita.speed(5)
+
+lentorra.speed(1)
+lentorra.left(180)
+lentorra.fd(100)
